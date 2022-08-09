2086d2b7c5bd0327343e8ef27cdd52bd4faeedd9
Laura Zheng <lyzheng@umd.edu>
2022-08-09 14:31:36 -0400

    modified training

--- drive.sh
+++ drive.sh
@@ -1,8 +1,8 @@
 export CARLA_ROOT=/home/laura/DrivingSimulators/CARLA_0.9.10           # change to where you installed CARLA
-export PORT=2000                                                    # change to port that CARLA is running on
+export PORT=3000                                                    # change to port that CARLA is running on
 export ROUTES=./leaderboard/data/routes_training/route_00.xml         # change to desired route
 export TEAM_AGENT=image_agent.py                                    # no need to change
-export TEAM_CONFIG=34.ckpt                                       # change path to checkpoint
+export TEAM_CONFIG=checkpoints/test_model/epoch=49.ckpt                                       # change path to checkpoint
 export HAS_DISPLAY=1                                                # set to 0 if you don't want a debug window
 export DEBUG_CHALLENGE=0
 
